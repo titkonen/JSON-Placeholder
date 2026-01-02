d761cc99a437efe08f01a8a57b9a547bb357bfd6
Toni Itkonen <kirijaki@icloud.com>
2026-01-02 08:12:26 +0200

    Loading view created

---
+++ JsonPlaceholder-1/View/LoadingView.swift
@@ -0,0 +1,25 @@
+//
+//  LoadingView.swift
+//  JsonPlaceholder-1
+//
+//  Created by Toni Itkonen on 2.1.2026.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Loading...")
+                .font(.largeTitle)
+            ProgressView()
+            Text("Loading content...")
+                .foregroundColor(Color.secondary)
+        }
+        
+    }
+}
+
+#Preview {
+    LoadingView()
+}
